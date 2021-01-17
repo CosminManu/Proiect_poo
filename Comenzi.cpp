e69433b16f67e447beb6ec4ddc764578897c332c
CosminManu <75031246+CosminManu@users.noreply.github.com>
2021-01-17 23:35:12 +0200

    Update Comenzi.cpp

--- Comenzi.cpp
+++ Comenzi.cpp
@@ -64,7 +64,18 @@ void Comenzi::DeterminareComanda()
 	{
 		if (this->com.at(1) == "INTO")
 		{
-			Insert();
+			if (this->com.at(3) == "VALUES")
+			{
+				Insert();
+			}			
+		}
+	}
+
+	if (this->com.at(0) == "SELECT")		
+	{
+		if (this->com.at(2) == "FROM" || this->com.at(4) == "FROM")
+		{
+			Select();
 		}
 	}
 
@@ -84,6 +95,102 @@ string Comenzi::getCom()
 	return com.at(0);
 }
 
+
+void Comenzi::Select()										
+{
+	fstream fisier;
+	fstream fisier1;
+	string line1;
+	int ok = 0, nr = 0, t = 0, i = 0, exit = 0, n = 0;
+	string line;
+	fisier.open("tabel.txt");
+
+	if (ok == 0)
+	{
+		for (string s : com)
+		{
+			if (s == "FROM")
+			{
+				i = 0;
+			}
+			if (i != 0)
+			{
+				t++;
+
+				if (t % 2 == 1)
+				{
+					vector_sel.push_back(s);		
+				}
+			}
+			if (s == "SELECT") {
+				i = 1;
+			}
+		}
+	}
+	n = vector_sel.size();
+	cout << endl;
+
+	while (getline(fisier, line))
+	{
+		nr++;
+		if ((nr % 3 == 2) || (nr % 3 == 0))
+		{
+			vector_col.push_back(line);
+		}
+	}
+
+
+	for (i = 0; i < vector_sel.size(); i++)
+	{
+		for (int j = 0; j < vector_col.size(); j++) 
+		{
+			if (vector_sel[i] == vector_col[j])		
+			{
+				ok = 0;
+				fisier1.open("insert.txt", ios::in);
+				while (getline(fisier1, line))
+				{
+					nr = 0;
+					if (ok == 0)
+					{
+						cout << endl;
+					}
+
+					if (line != "")
+					{
+						if ((ok > 0 && com.at(5) == line) || line == com.at(5))
+						{
+							ok++;
+							getline(fisier1, line);
+							nr++;
+							cout << "    " << line << "        ";
+							getline(fisier1, line);
+						}
+						nr++;
+						if (ok % 2 == 1 && nr != 1)
+						{
+							ok++;
+							cout << "    " << line << "        \n";
+						}
+						if (ok == 0)
+						{
+							ok = 2;
+						}
+
+					}
+				}
+				
+			}
+		}
+	}
+	
+	if (ok == 1)
+	{
+		cout << "\nColoana inexistenta";
+	}
+	fisier.close();
+}
+
 void Comenzi::DropTable()
 {
 	int ok = 0, nr = 0;
@@ -98,19 +205,16 @@ void Comenzi::DropTable()
 		{
 			if (nr < 3)
 			{
-				if (line == com.at(2) && ok == 0)		// DROP     TABLE     comenzi
-				{										// comat0   com,at(1) com.at(2)
+				if (line == com.at(2) && ok == 0)		
+				{										
 					line1 = line;
 					ok = 1;
 					nr++;
-					//fisier << line;
 				}
 				else
 					if (line1 == com.at(2))
 					{
 						nr++;
-						//line.replace(line.find(line), line.length(), "");
-						//fisier << line;
 					}
 			}
 			else {
@@ -132,19 +236,18 @@ void Comenzi::DropTable()
 			}
 		}
 
-		// !!!
-		/*fisier.open("tabel.txt", ios::out | ios::app | ios::trunc);
-		ifstream fisier1("drop.txt");
-		fisier1.open("drop.txt");*/
-
-
-		while (getline(fisier1, line))
+		ifstream fisierOut("drop.txt", ios::binary);
+		ofstream fisierIn("tabel.txt", ios::binary);
+		if (fisierOut.is_open() && fisierIn.is_open() && fisierIn.good() && fisierOut.good())
 		{
-			fisier << line << endl;
+			fisierIn << fisierOut.rdbuf();
+			fisierIn.close();
+			fisierOut.close();
 		}
+		
+		fisier1.open("drop.txt", ios::out | ios::trunc);
 		fisier1.close();
-		fisier.close();
-	
+
 		if (ok == 1)
 		{
 			cout << "    Tabela " << com.at(2) << " a fost stearsa" << endl;
@@ -157,12 +260,6 @@ void Comenzi::DropTable()
 		fisier.close();
 	}
 }
-
-// DROP TABLE comenzi --> drop.txt : abc litera cifra  
-//tabel.txt : comenzi nume varsta abc litera cifra
-//trunc = > sterge tabel.txt
-//muti drop > tabel.txt
-//     tabel.txt > tabel.txt
 
 void Comenzi::CreateTable()
 {
@@ -217,65 +314,78 @@ void Comenzi::CreateTable()
 		
 }
 
-void Comenzi::insertCol(vector<string> nume_coloana, string Tip, int dimensiune, string valoare_implicita)
-{
-	/*for (int i = 0; i < nume_coloana.size(); i++)
-	{
-		int nr_coloane = col->get_nr_coloane();
-		col[nr_coloane] = Coloana(nume_coloana, Tip, dimensiune, valoare_implicita);
-	}*/
-}
-
 void Comenzi::Insert()
 {
-	int i = 0, t = 0, ok = 0;
+	int i = 0, t = 0, ok = 0, gasit = 0;
 	string line;
 	fstream fisier;
-
-	if (ok == 0)
+	fstream fisier1;
+	
+	fisier1.open("tabel.txt", ios::in);
+	while (getline(fisier1, line))
 	{
-		for (string s : com)
+		if (com.at(2) == line)
 		{
-			if (s == ")")
+			gasit = 1;
+			if (ok == 0)
 			{
-				i = 0;
-			}
-			if (i != 0)
-			{
-				t++;
-
-				if (t % 2 == 1)
+				for (string s : com)
 				{
-					vector_coloane.push_back(s);
-				}
-			}
-			if (s == "(") {
-				i = 1;
-			}
-		}
-		fisier.open("insert.txt", ios::app);
-		
-		for (int i = 0; i < vector_coloane.size(); i++)
-		{
-			fisier << vector_coloane[i] << endl;
+					if (s == ")")
+					{
+						i = 0;
+					}
+					if (s == com.at(2))
+					{
+						vector_coloane.push_back(s);
+					}
+					if (i != 0)
+					{
+						t++;
 
+						if (t % 2 == 1)
+						{
+							vector_coloane.push_back(s);
+						}
+					}
+					if (s == "(") {
+						i = 1;
+					}
+				}
+				fisier.open("insert.txt", ios::app);
+
+				for (int i = 0; i < vector_coloane.size(); i++)
+				{
+					fisier << vector_coloane[i] << endl;
+				}
+				fisier.close();
+				cout << endl;
+				cout << "S-au inserat coloanele!" << endl;
+			}
+			if (gasit == 0)
+			{
+				break;
+			}
 		}
-		fisier.close();
-		cout << endl;
-		cout << "S-au inserat coloanele!" << endl;
+
 	}
+	if (gasit == 0)
+	{
+		cout << "Nu s au putut insera coloane pt ca tabela nu a fost creata.\n";
+	}
+	fisier1.close();
 }
 
-void Comenzi::DisplayTable()            // DISPLAY TABLE comenzi
+void Comenzi::DisplayTable()           
 {
 	cout << endl;
 
-	int ok = 1, nr = 0;
+	int ok = 1, nr = 0, exit = 0;
 	string line;
 	string line1;
 	fstream fisier1("insert.txt");
 	ifstream fisier("tabel.txt");
-	if (fisier.is_open())
+	if (fisier.is_open())			
 	{
 		while (getline(fisier, line))
 		{
@@ -294,56 +404,56 @@ void Comenzi::DisplayTable()            // DISPLAY TABLE comenzi
 		}
 		if (ok != 2)
 		{
-			cout << "Tabela nu exista";
+			cout << "\nTabela nu exista";
+			exit = -1;
 		}
-
 		fisier.close();
 	}
-
-	if (fisier1.is_open())
-	{	
-		int ok = 0;
-		while (getline(fisier1, line))
+	if (exit == 0)
+	{
+		if (fisier1.is_open())      
 		{
-			if (ok == 0) {
-				cout << endl;
-			}
-			if (line != "")
+			int ok = 0;
+			while (getline(fisier1, line))
 			{
-				ok++;
-				cout << "    " << line << "        ";
-			}
-			if (ok == 2)
-			{
-				cout << endl;
-				ok = 0;
-			}
-			
-		}
-	}
+				nr = 0;
+				if (ok == 0)
+				{
+					cout << endl;
+				}
 
-	else cout << "Nu s a putut deschide fisierul" << endl;
+				if (line != "")
+				{
+					if ((ok > 0 && com.at(2) == line) || line == com.at(2))
+					{
+						ok++;
+						getline(fisier1, line);
+						nr++;
+						cout << "    " << line << "        ";
+						getline(fisier1, line);
+					}
+					nr++;
+					if (ok % 2 == 1 && nr != 1)  
+					{
+						ok++;
+						cout << "    " << line << "        \n";
+					}
+					if (ok == 0)
+					{
+						ok = 2;
+					}
+
+				}
+
+
+			}
+		}
+		else cout << "\nNu s a putut deschide fisierul";
+	}
+	fisier1.open("insert.txt", ios::out | ios::trunc);
+	fisier1.close();
+	
 	cout << endl;
 }
 
 
-
-Tabela* Comenzi::getTab()
-{
-	return tab;
-}
-
-void Comenzi::Select()
-{
-
-}
-
-void Comenzi::Update()
-{
-
-}
-
-void Comenzi::Delete()
-{
-
-}
